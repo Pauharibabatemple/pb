9eae31980ed6646f794520332a09b7e368c5d39c
Pauhari Baba <95703538+Pauharibabatemple@users.noreply.github.com>
2023-05-06 23:55:04 +0530

    Add files via upload

---
+++ song-main/js/music-list.js
@@ -0,0 +1,19 @@
+
+
+let allMusic = [
+  
+ {
+    name: "Shree Hanuman Chalisa ",
+    artist: "T-Series",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Shiv Tandav Stotram ",
+    artist: "Shankar Mahadevan",
+    img: "music-1",
+    src: "music-1"
+  },
+  
+  
+];
