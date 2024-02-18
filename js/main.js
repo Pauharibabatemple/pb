9194a27c87c5534f1b1a761346ab87122e086c00
Pauhari Baba <95703538+Pauharibabatemple@users.noreply.github.com>
2024-02-18 17:31:24 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -279,3 +279,33 @@ $(document).ready(function(){
     });
   });
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+  const scriptURL = "https://script.google.com/macros/s/AKfycbw7Dk7OTiOWCge1qWXG0Xnt3R0Fak8qcgzE6felmgMyxLWU_Nb6LYPrT9t8VJYzMccHAQ/exec"
+  const form = document.forms['google-sheet']
+  const msg = document.getElementById("msg")
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => {
+
+        msg.innerHTML = "You subscribed successfully! "
+        setTimeout(function(){
+          msg.innerHTML = ""
+        },5000)
+        form.reset()
+      })
+      .catch(error => console.error('Error!', error.message))
+  })
+
